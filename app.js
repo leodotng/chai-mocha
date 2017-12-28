a288b206e426741d9da42baee3af8e59f4ead6d8
Josh <leong.jo@gmail.com>
2017-12-28 14:00:49 -0700

    unit tests

---
+++ app.js
@@ -0,0 +1,10 @@
+module.exports = {
+  sayHello: function(){
+    return 'hello';
+  },
+  addNumbers: function(value1, value2){
+    return value1 + value2;
+  }
+}
+
+//write a test to make sure the functions returns hello.
